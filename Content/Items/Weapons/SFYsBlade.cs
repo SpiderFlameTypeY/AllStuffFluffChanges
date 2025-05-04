5ea150f48c0f5a2ac332739bfb06ad1f9d299577
SpiderFlameTypeY <callofgame95@gmail.com>
2025-05-04 01:18:30 -0700

    update

--- Content/Items/Weapons/SFYsBlade.cs
+++ Content/Items/Weapons/SFYsBlade.cs
@@ -23,7 +23,7 @@ namespace AllStuffFluffChanges.Content.Items.Weapons
             Item.crit = 6;
 
             Item.value = Item.buyPrice(gold: 1);
-            Item.rare = ItemRarityID.LightRed;
+            Item.rare = ItemRarityID.Pink;
             Item.UseSound = SoundID.Item1;
         }
 
@@ -32,7 +32,7 @@ namespace AllStuffFluffChanges.Content.Items.Weapons
             if (Main.rand.NextBool(3))
             {
                 // Emit dusts when the sword is swung
-                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.);
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.174);
             }
         }
 
@@ -42,4 +42,10 @@ namespace AllStuffFluffChanges.Content.Items.Weapons
             target.AddBuff(BuffID.OnFire, 300);
         }
     }
+    
+    Recipe recipe = CreateRecipe();
+    recipe.AddIngredient(ItemID.Volcano);
+    recipe.AddIngredient(ItemID.SoulofMight, 20);
+    recipe.AddTile(TileID.MythrilAnvil);
+    recipe.Register();
 }
